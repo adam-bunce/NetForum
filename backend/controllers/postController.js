e83e4d312588a7c3caebd9a8c93f58841e90ee7e
adam <adam.bunce11@gmail.com>
2022-04-28 22:09:07 -0400

    testing setting post # in post controller

--- backend/controllers/postController.js
+++ backend/controllers/postController.js
@@ -42,10 +42,10 @@ const setPost = asyncHandler(async (req, res) => {
     
     // check if the postcount is accurate cause heroku going to sleep messes with it (resets it)
     const mostRecentPost = Post.find().limit(1).sort({$natural:-1})
-
+    console.log('----------------most recent postID: '+ mostRecentPost.postID)
 
     if (mostRecentPost.postID > parseInt(process.env.POSTCOUNT)){
-        
+
         process.env.POSTCOUNT = (mostRecentPost.postID).toString()
     }
 
